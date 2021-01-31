f36b0bcc671db7c8548c8868270cd5690bb08483
samanthaguernsey <samantha.guernsey@yahoo.com>
2021-01-31 15:39:16 -0800

    Unit 02

---
+++ Unit02/src/example/Trap.java
@@ -0,0 +1,15 @@
+package example;
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Samantha Guernsey
+//Date - Jan 31
+//Class - AP CSA
+//Lab  - Trap
+
+public class Trap
+{
+	public static double area( double base1, double base2, double height )
+	{
+		 
+		return (base1 + base2)/2 * height;
+	}
+}
---
+++ Unit02/src/example2/Cube.java
@@ -0,0 +1,13 @@
+package example2;
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return Math.pow(side,2) * 6;
+	}
+}
